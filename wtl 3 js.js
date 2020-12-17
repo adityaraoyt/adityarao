33e96230c315a27cb0faa32f20d7b1a3c3491518
adityaraoyt <62017113+adityaraoyt@users.noreply.github.com>
2020-12-17 22:28:34 +0530

    Add files via upload
    Html and js codes for form validation

---
+++ wtl 3 js.js
@@ -0,0 +1,12 @@
+
+
+function Verify(){	
+  var a1=document.getElementById("password"); 
+	var a2=document.getElementById("confpass");
+  if(a1!=a2){
+    window.alert("Try again.")
+  }
+  else{
+    pass;
+  }
+}
